748ba0bcfd5b3fb5267358fc0aa18ab1840a6f65
dovskyv <164265619+dovskyv@users.noreply.github.com>
2024-07-01 15:56:57 +0200

    Delete strona directory

--- strona/static/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#showContentLink').click(function(event) {
-        event.preventDefault(); // Zatrzymuje domyślne działanie linku
-        $('#initialContent').hide(); // Ukrywa napis "Kliknij aby wyświetlić"
-        $('#mainContent').show();   // Pokazuje resztę zawartości strony
-    });
-});
